1da42faec81489c64e4cbd2cf15ee0573aa047aa
Senel-Mac <senel.codes@gmail.com>
2025-09-24 10:16:53 +0900

    스토리보드 삭제, 초기 진입점 수정

---
+++ Anmin/Anmin/Scenes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Anmin
+//
+//  Created by Senel on 9/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemOrange
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
